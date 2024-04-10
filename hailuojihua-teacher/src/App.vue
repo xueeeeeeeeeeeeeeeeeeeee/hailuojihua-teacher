a5f4634f72cf640ffe4de87b3528cfeb1d1a53a1
xueeeeeeeeeeeeeeeeeeeee <122249998+xueeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2024-04-10 21:09:32 +0800

    header+menu

--- hailuojihua-teacher/src/App.vue
+++ hailuojihua-teacher/src/App.vue
@@ -1,21 +1,39 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import {  RouterView } from 'vue-router'
 import dayjs from 'dayjs';
 import "dayjs/locale/zh-cn";
 import locale from "ant-design-vue/es/locale/zh_CN";
+import Menuleft from "./components/Menuleft.vue"
+import Header from "./components/Header.vue"
 dayjs.locale("zh-cn");
+const collapsed = ref(false);
 
 </script>
 
 <template>
   <a-config-provider :locale="locale">
-    <div class="divheader">
-      <!-- <Header></Header> -->
-    </div>
-    <div class="body">
-      <RouterView />
+    <a-layout style="min-height: 100vh">
+    <a-layout-sider v-model:collapsed="collapsed" collapsible style="position: sticky; top: 0px; height: 100vh;">
+      <Menuleft></Menuleft>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0; position: sticky; top: 0px;">
+        <Header></Header>
+      </a-layout-header>
+      <a-layout-content style="margin: 0 16px">
+        <a-breadcrumb style="margin: 16px 0">
 
-    </div>
+        </a-breadcrumb>
+        <div :style="{ padding: '24px', background: '#fff', minHeight: '360px' }">
+          <RouterView />
+        </div>
+      </a-layout-content>
+      <a-layout-footer style="text-align: center">
+        Ant Design ©2018 Created by Ant UED
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
   </a-config-provider>
 </template>
 
---
+++ hailuojihua-teacher/src/components/Header.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="Header">
+    <div class="Header-left">
+      ipolicy
+    </div>
+    <div class="Header-right">
+<img src="../assets/dog.jpg" alt="">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.Header {
+  padding: 0px 30px 0px  ;
+  width: 100%;
+  height: 100%;
+  display: flex;
+justify-content: space-between;
+}
+.Header-right{
+  height: 40px;
+  width: 40px;
+}
+.Header-right img{
+  width: 100%;
+  height: 100%;
+  border-radius:50% ;
+}
+</style>
